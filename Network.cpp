3de09d6276f0cf6dc3286243616776ec99f3d634
Andrew Merrin <merrin101@gmail.com>
2022-02-01 16:58:35 +0000

    started adding files for firebase access

---
+++ Network.cpp
@@ -0,0 +1,10 @@
+#include "Network.h"
+
+define API_KEY ""
+define Firebase_Project_ID ""
+define USER_EMAIL ""
+define USER_PASSWORD ""
+
+void int main() {
+    return 0;
+}
---
+++ Network.h
@@ -0,0 +1,7 @@
+#ifdef Network_H_
+#define Network_H_
+
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+#endif
