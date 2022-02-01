64d56d2db657fb8beab0e9155fdeb69f3ea35f32
Andrew Merrin <merrin101@gmail.com>
2022-02-01 16:36:06 +0000

    Created Basic ros nodes (listener and subscriber) in C++

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/am/code/rosPractice/catkin_ws/devel/lib/beginner_tutorials/listener"
+  "/home/am/code/rosPractice/catkin_ws/devel/lib/beginner_tutorials/listener.pdb"
+  "CMakeFiles/listener.dir/src/listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/am/code/rosPractice/catkin_ws/src/beginner_tutorials
+# Build directory: /home/am/code/rosPractice/catkin_ws/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
